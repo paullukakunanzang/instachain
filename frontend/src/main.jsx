37bfec3c161a58450820038847c75596d12c00d6
manoahLinks <manoahluka@gmail.com>
2023-11-05 09:43:55 +0100

    feat: Adding datacontext provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { DataContextProvider } from './context/DataContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <DataContextProvider>
+        <App />
+      </DataContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 )
