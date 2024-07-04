a18fdd26b5a58c9e918f3b6adfa17ea1c2116916
manoahLinks <manoahluka@gmail.com>
2024-07-04 12:15:14 +0100

    chore: change address

--- frontend/src/components/InvestForm.jsx
+++ frontend/src/components/InvestForm.jsx
@@ -8,7 +8,7 @@ const InvestForm = () => {
             <h4 className='font-bold text-center text-white'>Invest</h4>
             <span className='text-white flex text-center gap-x-4'>
                 <small className='flex items-center gap-x-2 text-slate-400'><HiWallet/> wallet </small>  
-                <small className='text-[15px]'>bc1qzpk95lq5ks75u5k8mpwf0pluvuu5gdt7nd32qq</small>
+                <small className='text-[15px]'>35Hd5tza5Nh9S5hQbHa85HTV6a7EqnbzgS</small>
             </span>
 
             <img src={img2} className='' alt="" />
